205968607ff31cef00a69575dc6f60193d9ab079
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2026-01-09 14:56:51 +1100

    test: Implement a test using schema validation by Pydantic

--- tests/public_api/test_httpbin_basic.py
+++ tests/public_api/test_httpbin_basic.py
@@ -1,4 +1,5 @@
 import pytest
+from pydantic import BaseModel, HttpUrl
 
 pytestmark = pytest.mark.smoke
 
@@ -9,6 +10,27 @@ def test_httpbin_get_echoes_args(httpbin_client):
 
     body = r.json()
     assert body["args"]["q"] == "hello"
+
+
+class HttpBinResponse(BaseModel):
+    # Move these to `src/schemas/httpbin.py` later.
+    args: dict[str, str]
+    headers: dict[str, str]
+    origin: str
+    url: HttpUrl
+
+
+def test_httpbin_get_with_pydantic(httpbin_client):
+    test_param = 'pydantic_test'
+    response = httpbin_client.get('/get', params={'q': test_param})
+    assert response.status_code == 200
+
+    # Convert the JSON of the response body to the model.
+    # It's automatically validated here such as mandatory fields and types.
+    model = HttpBinResponse(**response.json())
+
+    assert model.args['q'] == test_param
+    assert str(model.url) == f"https://httpbin.org/get?q={test_param}"
 
 
 def test_httpbin_status_404(httpbin_client):
