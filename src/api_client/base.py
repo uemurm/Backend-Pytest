eb5dd7b9cd21178cc0e1d2cb3f89f1e4d2cf5773
Mitsu Uemura <mitsuhide.uemura@gmail.com>
2026-01-09 13:13:05 +1100

    chore: Print request/response details when failing

--- src/api_client/base.py
+++ src/api_client/base.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Any
 import requests
 
 
@@ -8,11 +9,25 @@ class BaseApiClient:
     session: requests.Session
     timeout_s: float = 10.0
 
+    last_request: dict[str, Any] | None = None
+    # This can be either Response or None. Initialise it to None.
+    last_response: requests.Response | None = None
+
     def _url(self, path: str) -> str:
         return self.base_url.rstrip("/") + "/" + path.lstrip("/")
 
     def get(self, path: str, **kwargs) -> requests.Response:
-        return self.session.get(self._url(path), timeout=self.timeout_s, **kwargs)
+        url = self._url(path)
+        self.last_request = {"method": "GET", "url": url, "kwargs": kwargs}
+        response = self.session.get(self._url(path), timeout=self.timeout_s, **kwargs)
+        self.last_response = response
+
+        return response
 
     def post(self, path: str, **kwargs) -> requests.Response:
-        return self.session.post(self._url(path), timeout=self.timeout_s, **kwargs)
+        url = self._url(path)
+        self.last_request = {"method": "POST", "url": url, "kwargs": kwargs}
+
+        response = self.session.post(url, timeout=self.timeout_s, **kwargs)
+        self.last_response = response
+        return response
